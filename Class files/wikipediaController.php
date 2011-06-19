9185564d9d99a4e8d08417f4bc2763d7a0eaea10
Abhinav Khanna <abhi1994@yahoo.com>
2011-06-19 09:48:57 -0700

    Changes to WikipediaController

--- Class files/wikipediaController.php
+++ Class files/wikipediaController.php
@@ -8,18 +8,32 @@
 class wikipediaController
 {
 	//protected properties here
+	protected $_apiURL;
+	protected $_dbConnection;
+	protected $_college;
 	
 	//methods here
-	public function __construct()
+	public function __construct($dbConnection, $college)
 	{
-		
+		$this->_dbConnection = $dbConnection;
+		$this->_college = $college;
+		$this->_apiURL = "http://en.wikipedia.org/w/api.php?";
 	}
 	
+	/**
+	 * public function wikiLinks
+	 * Postcondition: the links from wikipedia are stored into Database.
+	 */
 	public function wikiLinks()
 	{
 		
 	}
 	
+	
+	/**
+	 * Public function wikiPictures()
+	 * Postcondition: returns and stores the pictures in the Database.
+	 */
 	public function wikiPictures()
 	{
 		
