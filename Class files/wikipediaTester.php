457d094df33475e905fe7644b24543591ca7125c
Abhinav Khanna <abhi1994@yahoo.com>
2011-06-22 20:23:58 -0700

    Tester for Wiki File

---
+++ Class files/wikipediaTester.php
@@ -0,0 +1,12 @@
+<?PHP
+function __autoload($class)
+{
+	require_once $class . '.php';
+}
+
+$dbConnection = new dbConnections('lala', "localhost:8889", "root", "root");
+$wikipediaController = new wikipediaController($dbConnection, "Stanford_University");
+
+$wikipediaController->wikiPictures();
+
+?>
