8a3a38b78b17ff44fd1315c015e8cf4da4984d9e
Abhinav Khanna <abhi1994@yahoo.com>
2011-06-23 22:47:46 -0700

    added a updateTable method to relationalDbTester

--- Class files/relationalDbConnections.php
+++ Class files/relationalDbConnections.php
@@ -23,7 +23,7 @@ class relationalDbConnections extends dbConnections
 	{
 		$result = $this->selectFromTable($keyTable, $foreignKey, $valueForKey);
 		$num = mysql_num_rows($result);
-		if($num == 0)
+		if($num == 0 && ($tableName1 != $keyTable))
 		{
 			$array = array($foreignKey => "$valueForKey");
 			parent::insertIntoTable($keyTable, $array);
@@ -33,18 +33,53 @@ class relationalDbConnections extends dbConnections
 			parent::insertIntoTable($tableName1, $arrayOfValues);
 			return true;
 		}
-		else
+		elseif($num == 0 && ($tableName1 == $keyTable))
+		{
+			$arrayOfValues[$foreignKey] = $valueForKey;
+			parent::insertIntoTable($tableName1, $arrayOfValues);
+			return true;
+		}
+		elseif($tableName1 != $keyTable)
 		{
 			$formatted = $this->formatQueryResults($result, $primaryKey);
 			$arrayOfValues[$primaryKey] = $formatted[0];
 			parent::insertIntoTable($tableName1, $arrayOfValues);
+			return true;
+		}
+		else
+		{
+			return false;
 		}
 		
 	}
 	
-	public function updateTable()
+	/**
+	 * will update the Relational DB table; if trying to update the table containing the key, give both $tableName1 and $keyTable as the
+	 * 			same value.
+	 * @param: $tableName1: the table you wish to update
+	 * @param: $keyTable: the table containing the cross-table Key
+	 * @param: $foreignkey: the foreignKey your using to check;
+	 * @param: $valueForKey: the value for the foreignKey;
+	 * @param: $primaryKey: the primaryKey of the database (the cross-table Key);
+	 * @param: $arrayOfValues: the values being updated;
+	 * Postcondition: updates the table if entry already exists;
+	 * Postcondition: creates a new entry if entry does not exist;
+	 */
+	public function updateTable($tableName1, $keyTable, $foreignKey, $valueForKey, $primaryKey, $arrayOfValues, $condition)
 	{
+		$result = $this->selectFromTable($keyTable, $foreignKey, $valueForKey);
+		$num = mysql_num_rows($result);
 		
+		if($num == 0)
+		{
+			$this->insertIntoTable($tableName1, $keyTable, $foreignKey, $valueForKey, $primaryKey, $arrayOfValues);
+			return true;
+		}
+		else
+		{
+			parent::updateTable($tableName1, $arrayOfValues, $condition);
+			return true;
+		}
 	}
 	
 } // END class 
