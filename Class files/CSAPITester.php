ed1ba96634d96226ed02517313c6e82f6c2604a2
Rohit Sanbhadti <rohits47@gmail.com>
2011-07-21 10:27:37 -0700

    Added tester file for API, Majority of CSAPI completed.

---
+++ Class files/CSAPITester.php
@@ -0,0 +1,8 @@
+<?php
+// The function __autoload is the method for loading all the classes being used in the script. Use it at the beginning of every php main
+// page.
+function __autoload($class)
+{
+	require_once $class . '.php';
+}
+?>
